c04981ad725f72703b05a5a2a195878a720d1b66
ValkoHappy <11081108cat@gmail.com>
2023-05-30 17:13:54 +0300

    adjustment in the inventory code

---
+++ Assets/Inventory/Scripts/Item/ItemSlot.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public  abstract class ItemSlot : ISerializationCallbackReceiver
+{
+    [SerializeField] protected InventoryItemData InventoryItemData;
+    [SerializeField] protected int ItemId = -1;
+    [SerializeField] protected int StackSize;
+
+    public InventoryItemData ItemData => InventoryItemData;
+    public int Size => StackSize;
+
+    public void ClearSlot()
+    {
+        InventoryItemData = null;
+        ItemId = -1;
+        StackSize = -1;
+    }
+
+    public void AssignItem(InventorySlot inventorySlot)
+    {
+        if (ItemData == inventorySlot.ItemData)
+        {
+            AddToStack(inventorySlot.StackSize);
+        }
+        else
+        {
+            InventoryItemData = inventorySlot.ItemData;
+            ItemId = ItemData.Id;
+            StackSize = 0;
+            AddToStack(inventorySlot.StackSize);
+        }
+    }
+
+    public void AssignItem(InventoryItemData data, int amount)
+    {
+        if (ItemData == data)
+        {
+            AddToStack(amount);
+        }
+        else
+        {
+            InventoryItemData = data; 
+            ItemId = data.Id;
+            StackSize = 0;
+            AddToStack(amount);
+        }
+    }
+
+    public void AddToStack(int amount)
+    {
+        StackSize += amount;
+    }
+
+    public void RemoveFromStack(int amount)
+    {
+        StackSize -= amount;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        if (ItemId == -1)
+            return;
+
+        var dataBase = Resources.Load<DataBase>("Database");
+        InventoryItemData = dataBase.GetItem(ItemId);
+    }
+
+    public void OnBeforeSerialize()
+    {
+
+    }
+}
