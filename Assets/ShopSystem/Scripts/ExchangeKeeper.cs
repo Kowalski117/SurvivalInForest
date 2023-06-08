0abcddb59d9b256d5632752ca4af228e0d67389f
ValkoHappy <11081108cat@gmail.com>
2023-06-08 20:10:38 +0300

    correction in the trading system

--- Assets/ShopSystem/Scripts/ExchangeKeeper.cs
+++ Assets/ShopSystem/Scripts/ExchangeKeeper.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 [RequireComponent(typeof(UniqueID))]
 public class ExchangeKeeper : MonoBehaviour, IInteractable
 {
+    public static UnityAction<ExchangeKeeper> OnExchangeDisplayRequested;
     public UnityAction<IInteractable> OnInteractionComplete { get; set; }
 
     public void EndInteraction()
@@ -13,11 +14,11 @@ public class ExchangeKeeper : MonoBehaviour, IInteractable
 
     public void Interact(Interactor interactor, out bool interactSuccessfull)
     {
-        var playerInventory = interactor.GetComponent<PlayerInventoryHolder>();
+        var playerInventory = interactor.PlayerInventoryHolder;
 
         if (playerInventory != null)
         {
-            OnInteractionComplete?.Invoke(this);
+            OnExchangeDisplayRequested?.Invoke(this);
             interactSuccessfull = true;
         }
         else
--- Assets/ShopSystem/Scripts/UI/UIHandler.cs
+++ Assets/ShopSystem/Scripts/UI/UIHandler.cs
@@ -1,52 +1,142 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class UIHandler : MonoBehaviour
 {
+    [SerializeField] private InventoryPlayerInput _inventoryPlayerInput;
+    [SerializeField] private CursorController _cursorController;
+
+    [SerializeField] private DynamicInventoryDisplay _inventoryPanel;
+    [SerializeField] private DynamicInventoryDisplay _playerBackpackPanel;
+
     [SerializeField] private ExchangeHandler _shopKeeperDisplay;
-    [SerializeField] private ExchangeKeeper _shopKeeper;
     [SerializeField] private CraftingHandler _craftingHandler;
-    
+    [SerializeField] private CraftingHandler _craftingPlayerHandler;
+
+    private bool _isInventoryOpen = false;
+    private bool _isChestOpen = false;
+    private bool _isShopOpen = false;
+    private bool _isCraftOpen = false;
+    private bool _isCraftPlayerOpen = false;
+
     private void Awake()
     {
+        _inventoryPanel.gameObject.SetActive(false);
+        _playerBackpackPanel.gameObject.SetActive(false);
+
         _shopKeeperDisplay.gameObject.SetActive(false);
         _craftingHandler.gameObject.SetActive(false);
+        _craftingPlayerHandler.gameObject.SetActive(false);
     }
 
     private void OnEnable()
     {
+        InventoryHolder.OnDinamicInventoryDispleyRequested += DisplayInventory;
+        _inventoryPlayerInput.SwitchInventory += DisplayPlayerInventory;
+        _inventoryPlayerInput.OnCraftPlayerWindow += DisplayCraftPlayerWindow;
+
         CraftBench.OnCraftingDisplayRequested += DisplayCraftWindow;
-        _shopKeeper.OnInteractionComplete += DisplayShopWindow;
+        ExchangeKeeper.OnExchangeDisplayRequested += DisplayShopWindow;
     }
 
     private void OnDisable()
     {
+        InventoryHolder.OnDinamicInventoryDispleyRequested -= DisplayInventory;
+        _inventoryPlayerInput.SwitchInventory -= DisplayPlayerInventory;
+        _inventoryPlayerInput.OnCraftPlayerWindow -= DisplayCraftPlayerWindow;
+
         CraftBench.OnCraftingDisplayRequested -= DisplayCraftWindow;
-        _shopKeeper.OnInteractionComplete -= DisplayShopWindow;
+        ExchangeKeeper.OnExchangeDisplayRequested -= DisplayShopWindow;
     }
 
-    private void Update()
+    public void DisplayInventory(InventorySystem inventoryDislay, int offSet)
     {
-        if(_shopKeeperDisplay.gameObject.activeInHierarchy && Keyboard.current.escapeKey.wasPressedThisFrame)
+        _isChestOpen = !_isChestOpen;
+
+        if (_isChestOpen)
+        {
+            _inventoryPanel.gameObject.SetActive(true);
+            _cursorController.SetCursorVisible(true);
+            _inventoryPanel.RefreshDynamicInventory(inventoryDislay, offSet);
+        }
+        else
+        {
+            CloseChest();
+        }
+    }
+
+    public void DisplayPlayerInventory(InventorySystem inventoryDislay, int offSet)
+    {
+        _isInventoryOpen = !_isInventoryOpen;
+
+        if (_isInventoryOpen)
+        {
+            _playerBackpackPanel.gameObject.SetActive(true);
+            _cursorController.SetCursorVisible(true);
+            _playerBackpackPanel.RefreshDynamicInventory(inventoryDislay, offSet);
+        }
+        else
+        {
+            CloseInventory();
+        }
+    }
+
+    private void DisplayShopWindow(ExchangeKeeper exchangeKeeper)
+    {
+        _isShopOpen = !_isShopOpen;
+
+        if (_isShopOpen)
+        {
+            _shopKeeperDisplay.gameObject.SetActive(true);
+            _shopKeeperDisplay.CreateSlots();
+        }
+        else
         {
             _shopKeeperDisplay.gameObject.SetActive(false);
         }
+    }
 
-        if (_craftingHandler.gameObject.activeInHierarchy && Keyboard.current.escapeKey.wasPressedThisFrame)
+    private void DisplayCraftWindow(CraftingÑategory craftingÑategory)
+    {
+        _isCraftOpen = !_isCraftOpen;
+
+        if (_isCraftOpen)
+        {
+            _craftingHandler.gameObject.SetActive(true);
+            _craftingHandler.DisplayCraftingWindow(craftingÑategory);
+        }
+        else
         {
             _craftingHandler.gameObject.SetActive(false);
         }
     }
 
-    private void DisplayShopWindow(IInteractable interactor)
+    private void DisplayCraftPlayerWindow(CraftingÑategory craftingÑategory)
     {
-        _shopKeeperDisplay.gameObject.SetActive(true);
-        _shopKeeperDisplay.CreateSlots();
+        if(!_isCraftOpen && !_isShopOpen && !_isChestOpen)
+        {
+            _isCraftPlayerOpen = !_isCraftPlayerOpen;
+
+            if (_isCraftPlayerOpen)
+            {
+                _craftingPlayerHandler.gameObject.SetActive(true);
+                _craftingPlayerHandler.DisplayCraftingWindow(craftingÑategory);
+            }
+            else
+            {
+                _craftingPlayerHandler.gameObject.SetActive(false);
+            }
+        }
     }
 
-    private void DisplayCraftWindow(CraftBench craftBench)
+    private void CloseChest()
     {
-        _craftingHandler.gameObject.SetActive(true);
-        _craftingHandler.DisplayCraftingWindow(craftBench);
+        _inventoryPanel.gameObject.SetActive(false);
+        _cursorController.SetCursorVisible(false);
+    }
+
+    private void CloseInventory()
+    {
+        _playerBackpackPanel.gameObject.SetActive(false);
+        _cursorController.SetCursorVisible(false);
     }
 }
