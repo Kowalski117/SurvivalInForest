36c63b214dcfd5730e9c5847b35de9e8c004c581
ValkoHappy <11081108cat@gmail.com>
2023-06-25 00:07:45 +0300

    adjustment of the SurvivalHandler class

--- Assets/Source/SurvivalSystem/Scripts/SurvivalHandler.cs
+++ Assets/Source/SurvivalSystem/Scripts/SurvivalHandler.cs
@@ -5,6 +5,7 @@ public class SurvivalHandler : MonoBehaviour
     [SerializeField] private SurvivalAttribute _hunger;
     [SerializeField] private SurvivalAttribute _thirst;
     [SerializeField] private StaminaAttribute _stamina;
+    [SerializeField] private SurvivalAttribute _sleep;
     [SerializeField] private TimeHandler _timeHandler;
 
     public SurvivalAttribute Hunger => _hunger;
@@ -21,5 +22,6 @@ public class SurvivalHandler : MonoBehaviour
     {
         _hunger.LowerValue(_timeHandler.TimeMultiplier);
         _thirst.LowerValue(_timeHandler.TimeMultiplier);
+        _sleep.LowerValue(_timeHandler.TimeMultiplier);
     }
 }
