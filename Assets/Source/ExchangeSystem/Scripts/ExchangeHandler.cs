c4467ed0cc7bfa9ebe9ba75875583ce9662b3a62
ValkoHappy <11081108cat@gmail.com>
2023-06-27 22:04:44 +0300

    correction of defects

--- Assets/Source/ExchangeSystem/Scripts/ExchangeHandler.cs
+++ Assets/Source/ExchangeSystem/Scripts/ExchangeHandler.cs
@@ -1,18 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExchangeHandler : MonoBehaviour
 {
     [SerializeField] private PlayerInventoryHolder _inventoryHolder;
-
+    [SerializeField] private Transform _panel;
     [SerializeField] private Transform _containerForSlot;
     [SerializeField] private ExchangerItemList _exchangerItemList;
     [SerializeField] private ExchangerSlotView _exchangerSlotPrefab;
 
     [SerializeField] private Dictionary<InventoryItemData, int> _itemsHeld;
 
-    public void CreateSlots()
+    private bool _isShopOpen = false;
+
+    private void Start()
+    {
+        _panel.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        ExchangeKeeper.OnExchangeDisplayRequested += DisplayShopWindow;
+    }
+
+    private void OnDisable()
+    {
+        ExchangeKeeper.OnExchangeDisplayRequested -= DisplayShopWindow;
+    }
+
+    private void DisplayShopWindow(ExchangeKeeper exchangeKeeper)
+    {
+        _isShopOpen = !_isShopOpen;
+
+        if (_isShopOpen)
+        {
+            _panel.gameObject.SetActive(true);
+            CreateSlots();
+        }
+        else
+        {
+            _panel.gameObject.SetActive(false);
+        }
+    }
+
+    private void CreateSlots()
     {
         foreach (Transform child in _containerForSlot)
         {
