92428929c0979532247ce61c5ffd4c976a55159e
ValkoHappy <11081108cat@gmail.com>
2023-07-02 00:08:45 +0300

    creating melee weapons

--- Assets/Source/WeaponSystem/Scripts/Weapon.cs
+++ Assets/Source/WeaponSystem/Scripts/Weapon.cs
@@ -1,56 +1,131 @@
+using UnityEditor.Media;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Weapon : Raycast
 {
+    [SerializeField] private PlayerInventoryHolder _inventory;
+    [SerializeField] private HotbarDisplay _hotbarDisplay;
     [SerializeField] private WeaponPlayerInput _weaponPlayerInput;
     [SerializeField] private LayerMask _creatureLayer;
-    [SerializeField] private float _damage;
-    [SerializeField] private float _speedShoot;
-    [SerializeField] private ItemPickUp _bullet;
-    [SerializeField] private ParticleSystem _muzzleFlash;
-    [SerializeField] private ParticleSystem _hitEffect;
-    [SerializeField] private AudioClip _muzzleSound;
     [SerializeField] private AudioSource _audioSource;
 
+    private WeaponItemData _currentWeapon;
+
+    private Animals _currentAnim;
     private float _nextFire;
 
     private void OnEnable()
     {
-        _weaponPlayerInput.OnShoot += Shoot;
+        _weaponPlayerInput.OnShoot += UseWeapon;
+        _hotbarDisplay.ItemClicked += Init;
     }
 
     private void OnDisable()
     {
-        _weaponPlayerInput.OnShoot -= Shoot;
+        _weaponPlayerInput.OnShoot -= UseWeapon;
+        _hotbarDisplay.ItemClicked -= Init;
+    }
+
+    public void Init(InventoryItemData itemData)
+    {
+        if (itemData != null)
+        {
+            if (itemData is WeaponItemData weaponItemData)
+            {
+                _currentWeapon = weaponItemData;
+            }
+            else
+            {
+                _currentWeapon = null;
+                return;
+            }
+        }
+        else
+        {
+            _currentWeapon = null;
+            return;
+        }
+    }
+
+    private void UseWeapon()
+    {
+        var itemData = _hotbarDisplay.GetInventorySlotUI().AssignedInventorySlot.ItemData;
+
+        Init(itemData);
+
+        if (_currentWeapon != null && _currentWeapon.WeaponType == WeaponType.RangedWeapon)
+        {
+            Shoot();
+        }
+        else if (_currentWeapon != null && _currentWeapon.WeaponType == WeaponType.MeleeWeapon)
+        {
+            Hit();
+        }
     }
 
     private void Shoot()
     {
-        if(Time.time > _nextFire)
+        if (Time.time > _nextFire)
         {
-            _nextFire = Time.time + 1 / _speedShoot;
-
-            Debug.Log("ВЫСТРЕЛ");
-            _audioSource.PlayOneShot(_muzzleSound);
-            _muzzleFlash.Play();
-
-            if (IsRayHittingSomething(_creatureLayer, out RaycastHit hitInfo))
+            if (_inventory.InventorySystem.RemoveItemsInventory(_currentWeapon.Bullet.ItemData, 1))
             {
-                Vector3 spawnPoint = hitInfo.collider.ClosestPointOnBounds(hitInfo.point);
+                _nextFire = Time.time + 1 / _currentWeapon.Speed;
 
-                if (_bullet != null)
-                {
-                    ItemPickUp bullet = Instantiate(_bullet, spawnPoint, Quaternion.LookRotation(hitInfo.normal), hitInfo.collider.transform);
-                }
+                _audioSource.PlayOneShot(_currentWeapon.MuzzleSound);
+                //_currentWeapon.MuzzleFlash.Play();
 
-                if (hitInfo.collider.TryGetComponent(out Animals animals))
+                if (IsRayHittingSomething(_creatureLayer, out RaycastHit hitInfo))
                 {
-                    ParticleSystem impact = Instantiate(_hitEffect, spawnPoint, Quaternion.LookRotation(hitInfo.normal), hitInfo.collider.transform);
-                    impact.Play();
-                    Debug.Log("Попал в животное");
-                    animals.TakeDamage(_damage, 0);
+                    Vector3 spawnPoint = hitInfo.collider.ClosestPointOnBounds(hitInfo.point);
+
+                    if (hitInfo.collider.TryGetComponent(out Animals animals))
+                    {
+                        if(_currentWeapon.HitEffect != null)
+                        {
+                            ParticleSystem impact = Instantiate(_currentWeapon.HitEffect, spawnPoint, Quaternion.LookRotation(hitInfo.normal), hitInfo.collider.transform);
+                            impact.Play();
+                        }
+
+                        animals.TakeDamage(_currentWeapon.Damage, 0);
+                    }
+
+                    if (_currentWeapon.Bullet != null)
+                    {
+                        ItemPickUp bullet = Instantiate(_currentWeapon.Bullet, spawnPoint, Quaternion.LookRotation(hitInfo.normal), hitInfo.collider.transform);
+                    }
                 }
             }
+        }
+    }
+
+    private void Hit()
+    {
+        if (Time.time > _nextFire)
+        {
+            _nextFire = Time.time + 1 / _currentWeapon.Speed;
+            _audioSource.PlayOneShot(_currentWeapon.MuzzleSound);
+
+            if (_currentAnim != null)
+                _currentAnim.TakeDamage(_currentWeapon.Damage, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Animals animals))
+        {
+            if (animals != null)
+                _currentAnim = animals;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out Animals animals))
+        {
+            if (animals != null)
+                _currentAnim = null;
         }
     }
 }
