e5b1d5326d7a44aa911d13cbfeddbebecfdcdd19
Kowalski117 <tavrin09@gmail.com>
2023-07-29 01:57:20 +0300

    Optimization

---
+++ Assets/ButtonScenes.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScenes : MonoBehaviour
+{
+  public void StartSceneForest()
+  {
+    SceneManager.LoadScene(1);
+  }
+}
