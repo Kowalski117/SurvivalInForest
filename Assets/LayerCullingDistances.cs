2857e7c2760f0f992502a44c96f9c7ad2f181694
Kowalski117 <tavrin09@gmail.com>
2023-09-26 21:47:48 +0300

    Reduced the number of stones

--- Assets/LayerCullingDistances.cs
+++ Assets/LayerCullingDistances.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class LayerCullingDistances : MonoBehaviour
 {
-    [SerializeField]private Camera _camera;
-    [SerializeField]
-    private float[] _cullingDistances = new float[32];
-    
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float[] _cullingDistances = new float[32];
 
+    private int _minDistances = 50;
+    private int _maxDistances = 700;
     private void Awake()
     {
         SetCullingDistances();
@@ -21,5 +21,29 @@ public class LayerCullingDistances : MonoBehaviour
     private void SetCullingDistances()
     {
         _camera.layerCullDistances = _cullingDistances;
+    }
+
+    public void IncreaseDistance()
+    {
+        if (_cullingDistances[0] < _maxDistances)
+        {
+            for (int i = 0; i < _cullingDistances.Length; i++)
+            {
+                _cullingDistances[i] = _cullingDistances[i] * 2;
+            }
+            SetCullingDistances();
+        }
+    }
+
+    public void DecreaseDistance()
+    {
+        if (_cullingDistances[0] > _minDistances)
+        {
+            for (int i = 0; i < _cullingDistances.Length; i++)
+            {
+                _cullingDistances[i] = _cullingDistances[i] / 2;
+            }
+            SetCullingDistances();
+        }
     }
 }
