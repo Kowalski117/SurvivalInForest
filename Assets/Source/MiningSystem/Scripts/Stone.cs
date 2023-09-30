bc0c522ab0af1d722f6f487dbe4bf287fadf33ac
Kowalski117 <tavrin09@gmail.com>
2023-09-30 21:45:36 +0300

    Change Animals

--- Assets/Source/MiningSystem/Scripts/Stone.cs
+++ Assets/Source/MiningSystem/Scripts/Stone.cs
@@ -1,17 +1,5 @@
-using DG.Tweening;
-using Tayx.Graphy.Utils.NumString;
-using UnityEngine;
-
 public class Stone : Resource
 {
-    private float _maxHealth;
-
-    public override void OnEnable()
-    {
-        base.OnEnable();
-        _maxHealth = Health;
-    }
-    
     public override void Die()
     {
         Сollider.enabled = false;
