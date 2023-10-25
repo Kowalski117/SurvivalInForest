bc976fd05aee04c3bc611cc55f3769cfbb7b07bf
ValkoHappy <11081108cat@gmail.com>
2023-10-25 23:32:28 +0300

    changing the scene

--- Assets/ButtonScenes.cs
+++ Assets/ButtonScenes.cs
@@ -10,10 +10,19 @@ public class ButtonScenes : MonoBehaviour
         _loadPanel.gameObject.SetActive(true);
         _loadPanel.Load(1, () => SceneManager.LoadScene(1), 1);
     }
-    
-        public void StartSceneCave()
-        {
-            _loadPanel.gameObject.SetActive(true);
-            _loadPanel.Load(1, () => SceneManager.LoadScene(2), 1);
-        }
+
+    public void StartSceneCave()
+    {
+        _loadPanel.gameObject.SetActive(true);
+        _loadPanel.Load(1, () => SceneManager.LoadScene(2), 1);
+    }
+
+    public void StartLastSaveScene()
+    {
+        _loadPanel.gameObject.SetActive(true);
+
+        int indexScene = ES3.Load<int>("SceneIndex");
+
+        _loadPanel.Load(1, () => SceneManager.LoadScene(indexScene), indexScene);
+    }
 }
