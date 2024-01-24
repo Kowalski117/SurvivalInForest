3b364f519f606c0de44b8832f27f61edc2f0b665
ValkoHappy <11081108cat@gmail.com>
2024-01-24 19:10:07 +0300

    adding new constants

--- Assets/Source/Constants/Scripts/SettingConstants.cs
+++ Assets/Source/Constants/Scripts/SettingConstants.cs
@@ -8,6 +8,7 @@ public class SettingConstants : MonoBehaviour
     public const string BoolStr = "Bool";
     public const string Sensitivity = "Sensitivity";
     public const string AutomaticCollectionOfItems = "AutomaticCollectionOfItems";
+    public const string TipsInput = "TipsInput";
     public const string MovementOfTrees = "MovementOfTrees";
     public const string LowTextureWater = "LowTextureWater";
     public const string HightTextureWater = "HightTextureWater";
